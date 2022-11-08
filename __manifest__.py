04f46807c85c71561840ed9ff23f8818cf2d6afd
a21angelma <a21angelma@localhost>
2022-11-08 10:58:34 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "odoo_olamundo",
+
+    'summary': """
+        Proxecto Inicial Ola Mundo
+    """,
+
+    'description': """
+        Primer proyecto en python para odoo
+    """,
+
+    'author': "a21angelma",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/olamundo.xml',
+        'views/templates.xml',
+        'views/menu.xml',
+        'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
